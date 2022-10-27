6ce447e2ccd1590822c6bfe0e77f4a05d258db18
Osuke Ishihara <s21c1012ll@s.chibakoudai.jp>
2022-10-27 12:30:59 +0900

    Test script

--- test.bash
+++ test.bash
@@ -1,5 +1,17 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2022 Osuke Ishihara
+# SPDX-License-Identifier: BSD-3-Clause
+
+ng () {
+	echo ${1}行目が違うよ
+	res=1
+}
+
+res=0
+
 
 out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng ${LINENO}
 
-[ "${out}" = 14 ]
+[ "$res" = 0 ] && echo OK
+exit $res
